ed427856d2fc4f2efd7c4dfde8d84e9da8c3146c
Chris Santamaria <chris@santamaria.me>
2019-06-24 12:01:35 -0400

    Add Home page and organize sidebar

---
+++ doczrc.js
@@ -0,0 +1,9 @@
+export default {
+    themeConfig: {
+        mode: 'light'
+    },
+    menu: [
+        'Home',
+        { name: 'Material-UI', menu: ['Getting Started', 'Styling', 'Icons'] }
+    ]
+};
